cadfbb301b0524b682d4c02194a01804613963ce
Blackstock <charlieblackstock@gmail.com>
2015-07-10 17:17:26 -0600

    added JS answers for problems 1-7

---
+++ main.js
@@ -0,0 +1,45 @@
+//answer for #1
+for (var a = 0; a <= 1000; a += 100) {
+  console.log(a);
+}
+
+//answer for #2
+for (var b = 1; b <= 128; b *= 2) {
+  console.log(b);
+}
+
+//answer for #3
+for (var c = 0; c <= 10; c += 2) {
+  console.log(c);
+}
+
+//answer for #4
+for (var d = 3; d <= 15; d += 3) {
+  console.log(d);
+}
+
+//answer for #5
+for (var e = 9; e >= 0; e--) {
+  console.log(e);
+}
+
+//answer for #6
+for (var f = 1; f <= 4; f += 1) {
+  console.log(f);
+  console.log(f);
+  console.log(f);
+// }
+
+//answer for #7
+for (var g = 0; g <= 4; g++) {
+    console.log(g);
+}
+for (var g = 0; g <= 4; g++) {
+    console.log(g);
+}
+for (var g = 0; g <= 4; g++) {
+    console.log(g);
+}
+for (var g = 0; g <= 4; g++) {
+    console.log(g);
+}
